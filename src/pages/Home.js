4923d2c8c4155fb93a9b7d0f68c85fe8911f7160
fiona <fiona.kitchen@yahoo.co.uk>
2022-11-02 12:12:19 +0000

    i have only changed one thing, i just need to put it down and walk away

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 function Home (){
+    const { user } = useAuth0();
+    const { name, } = user;
 
-
-    return(     <h2>It's time to water, you dumb bitch</h2>)
+    return(    <div> <h2>It's time to water, {name} you dumb bitch</h2>
+  </div>)
 }
 
 export default Home
